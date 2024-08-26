303568ce1ee6ff7c661a3fb202928da6131ccf91
nbutenko <nadea.butenko@gmail.com>
2024-08-26 01:54:43 -0500

    add api validation schema

---
+++ api/src/routes/video/schema.ts
@@ -0,0 +1,29 @@
+import joi from 'joi'
+
+export const idSchemaKey = {
+  id: joi.number().required()
+}
+
+const videoSchemaKeys = {
+  name: joi.string().required(),
+  artist: joi.string().required(),
+  url: joi.string().uri().required(),
+  poster: joi.string().uri().required()
+}
+
+export const idSchema = joi.object().keys(idSchemaKey)
+
+export const videoRequestSchema = joi.object({
+  ...videoSchemaKeys
+})
+
+export const videoResponseSchema = joi.object({
+  ...idSchemaKey,
+  ...videoSchemaKeys
+})
+
+export const allVideosResponseSchema = joi.array().items(videoResponseSchema)
+
+export const searchRequestSchema = joi.object({
+  search: joi.string().allow('', null)
+})
