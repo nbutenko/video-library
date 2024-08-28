b1040a111e857cab1d3d9e75c41ce1d196a569ab
nbutenko <nadea.butenko@gmail.com>
2024-08-27 22:51:10 -0500

    setup video mocks

---
+++ api/src/test-utils/video-mocks.ts
@@ -0,0 +1,83 @@
+export const mockVideos = [
+    {
+      id: 1,
+      name: 'Over It',
+      artist: 'Summer Walker',
+      url: 'https://www.youtube.com/watch?v=1A0CuoluUr0',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/a0dd/006c/9559239037eb3fd6899892cc2fec7f46?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=HtfKY9Bq1CtgJ-qCXSyX~sOVVeTHf9sKe5RSdZ2izKJr7FsbHO~2tvKed5eF55jBXLPlvEj91xjUk6VE6jn406uIe~UipM7Q2z1ZWopoZNcQBXYGoOWG30HGpjcwTaY4PUpwuqE~uXifKLt6AA-88RXnJsXi1yWt37GHZQVmkFbNeWsPh33VXGNGCdOEwtNehyPFljgM-FJPGnLh~~K50wkozIQNaYPx-fDlzdJVcnaF4Zj6zwieO2416xzNvKJm3O71KdegyU1zK8s4PUd0qYLZ5m~XVAfnEwIZNvVqIXuh6g6RcL0Au6LbF5yROZI-mXnCUNW73WTAusSXLx1iKA__'
+    },
+    {
+      id: 2,
+      name: 'Smells Like Teen Spirit',
+      artist: 'Nirvana',
+      url: 'https://www.youtube.com/watch?v=hTWKbfoikeg',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/e388/c4be/e4b7b0c99c7936569f979ff801b5af18?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a0t~ol0YZ5B~ZNS0ibq1rqyxxiGPzVckquDgR5iA0V3na2ybySpZhqI1VXfGBBCUVYs8ztvqniIUpwJpYPMXrnu5c1MThr1OyQKw44uut0S8JANdKGR9Vje5lff-P9~j-GUVyr-VXhws~Rx6ECl9dZdB8JLKh7LF1Ik4pOCRinXt~Y1YfETmJ4AAXLOZf0vSa4MnSy0FrWnv4SeU53EiqyK15mTDcKobfRecgGs1s2Au7z7I5MlYK3bj6ZT4z0KNGHpt5pF3DyN5dF4K6-91bb7pnO6pkRMYcVSuJy~5Rk43N1anrtMtw~SW70sHWf7o9GelI~rkzH9WFTb2KZqDAA__'
+    },
+    {
+      id: 3,
+      name: 'Amalia(Deuxe Version)',
+      artist: 'Doja Cat',
+      url: 'https://www.youtube.com/watch?v=r52KqG4G678',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/3594/e46e/cf397f663f238f0daa3109d978c3ecb2?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=jKspX06zdojpBN5tsqS1~dYUi9izR7oWAY5cZise33CR~nuNf7JL5D~frZuy8279JUbUnGZLSt0PKSkS8~H5WGcYoR~9w5a9Lv3hCfq4FAaNEwfO9w1NzfeK98a8MM1qo~k047DHj1bw-uGfoPTwn-gEGuerxHKcuSgC~jnGs4CSQzPR8rOP8L32UQhC2s4jyw-T8H5qXZnC03mmT6jQCnx~0IHzFUPpn-NZ-SeuLQfeessck3Ip0rCQXmdrw-Qr-E8oBuTSpXZSbNozt0xB~Yg278G-jWqQgWIELnNvLzQYJT1d4B0z0tcYgbGCYvuayn-jJiL8cMVqwTnHhKrfjA__'
+    },
+    {
+      id: 4,
+      name: 'Fever',
+      artist: 'megan Thee Stallion',
+      url: 'https://www.youtube.com/watch?v=PiZjlHvBu4U',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/753b/2344/d4c24345528304ac176035ee5b98bd53?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=IIKNSTNOcC5bUJhy1BKPOPcWCsfxxDDqD8wAxEiIcve-~JCecLodw-K3Mg8GekMOsT6c~XH~~OyAdy8XznG4smKEXZFJxC1Vlgyy5H~B75T8F6nPNs7L6FxJdodLYuLSwfgaMkUuQTYaVDd5s0rN1f4q717au0RkpvyH5x2-FHAJDvrrfozZFoz0dYv7sCocRR48sHQJFpyV55zYiDrqMClAl10Es20yMjITLgRAHcQm-N3~aYNzi77IY8EYzVY3y6VC7q5h2OmNa~8zYjN~tYrbojff8uy72l~D4LsMoqam7J~MuABFIWlRiWghrnheD9T12dnsOcQJzZrAjq8Aag__'
+    },
+    {
+      id: 5,
+      name: 'Certified Lover Boy',
+      artist: 'Drake',
+      url: 'https://www.youtube.com/watch?v=THVbtGqEO1o&list=PLgpnN8mfan3whGpgojRyDdQl7G7cEq3ex',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/767f/3836/607eed95df51e501aa710780a0bfa312?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=pB2tidgVeUFAZhgg8zuDsKXUVyU~GnRBRWL0CpbbD5C0Gxu1WLZGi85nRcNz1eOsemnXFWL7ZT6cZ-wUtFvnaCiblCT3qy~2i-NBRnK32hYSU~HyUFA4H32ft9RAtQrsNHO2eEv8d4lMYOL50wglJdX5d3o3p4DepVllG8yuWBKQYm4O6YrqZywswT0iD~usoItsHNUjtJf6oo~hL5icPOl7SuDA4TPfuI5enLvoYA~lbImgiEDuIxTRPO7gAUlNWkiSOgT0QJy-A~34o3ZJCy3ZNamP~kAy9Rnx---6mgkRVNW~FtjeyKJigoXqM~q3mvNOqyklflZnNTgoGYNsjw__'
+    },
+    {
+      id: 6,
+      name: 'Dawn FM',
+      artist: 'The Weeknd',
+      url: 'https://www.youtube.com/watch?v=w8eFZoOcYKc&list=PLtdokPm7vPss1TXrV4gr2pnSXmMu-UFF4',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/be57/dce7/2d35b7465a148f872c12d5ca7c0e1413?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=UAEvBxzzZhzGa5RqkcZHVp926q6GvgSXiqVA3F7sSuOd4CUgjAd8hfGlVsMnEZkD9fy3Aozo3YeIUyCMboDYPRDfwfttBxLx-vUjsZ4cuOiyYN4gseinvSpxALkBkLfb9veNVKBmAeXdrQ~zaRxIm6DrrT9KDK-G8J53iQj297uFWiusCoVtWrxD0EX9vqH4HGi-mEGLhOcb-6TCf6P7IJirt86R9rXSEu3UrWmYIlaEmzAxZigUlEiRlpon9djKD6OLxwZVtVlfZtgNGhCXAU6HXmARX9xMstSXuzAqTqL-vcbG50JcCY3okGuCPcjM5V7LJAxK~~bGJPmq1BxIbg__'
+    },
+    {
+      id: 7,
+      name: 'Meet The Woo',
+      artist: 'Pop Smoke',
+      url: 'https://www.youtube.com/watch?v=KHEzO3Vx3Oc',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/09ac/fc78/55b44c9dcb1907c509e89d31c6218af6?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=k89aZ1F5HKYO4mJAVqhRh34ABeP-Lbaca-SSOyt5DSe~AYqCXz1H~1UBCig~pMnElMm-tyTCEZgvp6U66UOE6fFMas7EfLyAMSWGl2nbuvejV8rPfyBsRuXJkNEPaozFZHxKPtUuUahe3Ut2qJnRBLosN6qIRLHPoBIycglZ09wNQI67iKYpE4PDoy6Rm-LV4FoLcnL3bLCUs3wPf4cMVrew5FB3FWY1JhiEvHa7aOnjYPSwVQePwwBjLTmHHcyT0rkTg4inyiC-gF9PTthwy5gguaUVxmiWW0ch85jmaW1FNlkiGKbUIeXY5twDKpbf2if25A1C4k1YO3Vpxw8RSA__'
+    },
+    {
+      id: 8,
+      name: 'Queen',
+      artist: 'Nicki Minaj',
+      url: 'https://www.youtube.com/watch?v=eMymSXUN9nQ',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/5314/2812/5cd3067ce4229579408fc6109345d1eb?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ozYMR-LXkDWV8zS8Qr-KjCghKakDz7vcXGpwcxAmKu2ro1DMrHonPOEFyfGnrnaZ-0bVLwN3Pz2byl2U804wlaLeoGXbj0BHahL1JOOafQA5RzOBc7g0SgIPEYslKf4j1SuCeebOk0Q-hfJekGI4W6jAtZd8EO9x5pN4yzL3X44oyNmqDb0C0e10cZGrCTjoUTSpBlfyLgnR88H25VsJdDJZI7ofnrLY3o7Ps12eobYvSC0kXog7XqMeRqURYJYH6XqiISwD~MNTUsvvHGFWTSBBKlVoWiM803zPLY~fakj8wX2pcpLFFHas3QW0WHfv4BeLi3iAgmR4CQSQErmBsQ__'
+    },
+    {
+      id: 9,
+      name: 'Issa',
+      artist: '21 Savage',
+      url: 'https://www.youtube.com/watch?v=EahkDoT1adg',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/8802/ffe4/fd4df55957ceaacf6bac192785de22ea?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=FhGkzKxaGPXAnHzXaGzTdoh3vtDlaGPls2TwicIaVU4mVasEc68cB~gR~Ba9tv4BDNC7Vfrtk0Cgi1X64UEOSnncdyoC9RRdfjSpx1FlJ3l~EQzZFUOAzm85rtL-7FHYNh386HfIIXxR9DjqQy51AIKQCFU4AvPZF4Zkg3vxHT-EyYGrqQ2BQO9tbFjD~REOtkDObIU6X7p0gIPGNBNr1SNNOTnK7JL1Aw2~arzbS~j77FNdjVjKvkB-KUyOXcHUQWXCu0QesKMoPbO8RG3PK8PhODIx2smWHvRQU6F0Hcq3la7v6T8T401jTIHIOEKWR8HGdQo7e2Cq4A27mWQUNA__'
+    },
+    {
+      id: 10,
+      name: 'Plain Jane REMIX (feat. Nicki Minaj) - Single',
+      artist: 'A$AP Ferg',
+      url: 'https://www.youtube.com/watch?v=YTqtH9onHUU&t=1s',
+      poster:
+        'https://s3-alpha-sig.figma.com/img/4ee2/d53f/28003339a175398818674f75dedc971b?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=Hit6iKcGc08x4WpteKdvsJWXGmoYJSM63iZGD7ZN5EDzM9TaEVqw3AzTjL~6Sh0QbxCL64adLjL1vXIb5blRX~A1c~nQTQ3CXus6te0SRXZ9CcNcw8Co8paNqULHXr54bkglaEHazBTXrVSF0Q3PKZz-LCicRwwEL5u~DGKyT46OEU618DU3UJfZ6wvil14MH90P3jSOOPrwzDTrOCTMy6g78vR6HcjLpHXDbaVQGAmiMyNZfzgTA51h25fyJvHgANeYnJjFBy1eN5EBAhXbRqwX4ntv6RyOr~RBVWiQhOu5TETa3dDEKaV4O6WNne3O6W05fnWG4e0tNHwZDjMnFA__'
+    }
+  ]
+  
